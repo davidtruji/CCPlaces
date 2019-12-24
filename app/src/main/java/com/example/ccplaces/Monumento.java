611db40091443038508c0531adbb2302cd75ee52
David T <davidtrujitorres@gmail.com>
2019-12-24 10:50:13 +0100

    Clase Monumento convertida en Entity para su uso en Room

--- app/src/main/java/com/example/ccplaces/Monumento.java
+++ app/src/main/java/com/example/ccplaces/Monumento.java
@@ -3,6 +3,9 @@ package com.example.ccplaces;
 import android.graphics.Bitmap;
 
 import androidx.annotation.NonNull;
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
 
 import java.util.Objects;
 
@@ -11,107 +14,139 @@ import java.util.Objects;
 //}
 
 
+@Entity(tableName = "monumento")
 public class Monumento {
 
+    @PrimaryKey(autoGenerate = true)
+    private int id;
 
+    @NonNull
     private String nombre;
-    private String tipo;
-    private String desc;
-    private Bitmap foto;
-    private Double lat;
-    private Double lon;
-    private Boolean fav;
+
+    private Boolean favorito;
 
 
-    public Monumento(String nombre) {
-        this.nombre = nombre;
+
+    public Monumento(String nombre){
+        this.nombre=nombre;
+        favorito=false;
     }
 
-
-
-    public Monumento(String nombre, String tipo, String desc, Bitmap foto, Double lat, Double lon) {
-        this.nombre = nombre;
-        this.tipo = tipo;
-        this.desc = desc;
-        this.foto = foto;
-        this.lat = lat;
-        this.lon = lon;
-        this.fav = false;
-    }
-
-    public String getTipo() {
-        return tipo;
-    }
-
-    public void setTipo(String tipo) {
-        this.tipo = tipo;
-    }
-
-    public String getDesc() {
-        return desc;
-    }
-
-    public void setDesc(String desc) {
-        this.desc = desc;
-    }
-
-    public Bitmap getFoto() {
-        return foto;
-    }
-
-    public void setFoto(Bitmap foto) {
-        this.foto = foto;
-    }
-
-    public Double getLat() {
-        return lat;
-    }
-
-    public void setLat(Double lat) {
-        this.lat = lat;
-    }
-
-    public Double getLon() {
-        return lon;
-    }
-
-    public void setLon(Double lon) {
-        this.lon = lon;
-    }
-
+    @NonNull
     public String getNombre() {
         return nombre;
     }
 
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
+    public Boolean getFavorito() {
+        return favorito;
     }
 
-    public Boolean getFav() {
-        return fav;
-    }
-
-    public void setFav(Boolean fav) {
-        this.fav = fav;
-    }
-
-
-    @NonNull
     @Override
     public String toString() {
         return nombre;
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Monumento monumento = (Monumento) o;
-        return Objects.equals(nombre, monumento.nombre);
-    }
+//    private String tipo;
+//    private String desc;
+//    private Bitmap foto;
+//    private Double lat;
+//    private Double lon;
+//    private Boolean fav;
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(nombre);
-    }
+
+
+
+
+
+//    public Monumento(String nombre) {
+//        this.nombre = nombre;
+//    }
+
+
+
+//    public Monumento(String nombre, String tipo, String desc, Bitmap foto, Double lat, Double lon) {
+//        this.nombre = nombre;
+//        this.tipo = tipo;
+//        this.desc = desc;
+//        this.foto = foto;
+//        this.lat = lat;
+//        this.lon = lon;
+//        this.fav = false;
+//    }
+//
+//    public String getTipo() {
+//        return tipo;
+//    }
+//
+//    public void setTipo(String tipo) {
+//        this.tipo = tipo;
+//    }
+//
+//    public String getDesc() {
+//        return desc;
+//    }
+//
+//    public void setDesc(String desc) {
+//        this.desc = desc;
+//    }
+//
+//    public Bitmap getFoto() {
+//        return foto;
+//    }
+//
+//    public void setFoto(Bitmap foto) {
+//        this.foto = foto;
+//    }
+//
+//    public Double getLat() {
+//        return lat;
+//    }
+//
+//    public void setLat(Double lat) {
+//        this.lat = lat;
+//    }
+//
+//    public Double getLon() {
+//        return lon;
+//    }
+//
+//    public void setLon(Double lon) {
+//        this.lon = lon;
+//    }
+//
+//    public String getNombre() {
+//        return nombre;
+//    }
+//
+//    public void setNombre(String nombre) {
+//        this.nombre = nombre;
+//    }
+//
+//    public Boolean getFav() {
+//        return fav;
+//    }
+//
+//    public void setFav(Boolean fav) {
+//        this.fav = fav;
+//    }
+//
+//
+//    @NonNull
+//    @Override
+//    public String toString() {
+//        return nombre;
+//    }
+//
+//    @Override
+//    public boolean equals(Object o) {
+//        if (this == o) return true;
+//        if (o == null || getClass() != o.getClass()) return false;
+//        Monumento monumento = (Monumento) o;
+//        return Objects.equals(nombre, monumento.nombre);
+//    }
+//
+//    @Override
+//    public int hashCode() {
+//        return Objects.hash(nombre);
+//    }
 }
--- app/src/main/java/com/example/ccplaces/OpenDataAPI.java
+++ app/src/main/java/com/example/ccplaces/OpenDataAPI.java
@@ -26,11 +26,11 @@ public class OpenDataAPI {
 
             for(int i=0; i< jsonArray.length();i++){
                 nombreMonumento  = jsonArray.getJSONObject(i).getJSONObject("rdfs_label").getString("value");
-                tipo= jsonArray.getJSONObject(i).getJSONObject("om_tipoMonumento").getString("value");
-                lat= jsonArray.getJSONObject(i).getJSONObject("geo_lat").getString("value");
-                lon= jsonArray.getJSONObject(i).getJSONObject("geo_long").getString("value");
-                monumentoArrayList.add(new Monumento(nombreMonumento,tipo,"desc",null,Double.valueOf(lat),Double.valueOf(lon)));
-                Log.i(TAG," Creado: "+nombreMonumento+" - "+tipo+" - "+lat+" "+lon);
+//                tipo= jsonArray.getJSONObject(i).getJSONObject("om_tipoMonumento").getString("value");
+//                lat= jsonArray.getJSONObject(i).getJSONObject("geo_lat").getString("value");
+//                lon= jsonArray.getJSONObject(i).getJSONObject("geo_long").getString("value");
+                monumentoArrayList.add(new Monumento(nombreMonumento));
+                Log.i(TAG," Creado: "+nombreMonumento);
             }
 
         } catch (JSONException e) {
